40e86224c6b179bb2963ca2898c8724ad873d23e
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2021-08-12 01:48:33 +0200

    Update script.js
    If applied, this commit will have more pokemon characters and display it using loop

--- js/script.js
+++ js/script.js
@@ -14,5 +14,28 @@ pokemonlist = [
     name: 'Clefairy',
     height: 6,
     types: 'Fairy'
+  },
+  {
+    name: 'Spearow',
+    height: 3,
+    types: ['Flying', 'Normal']
+  },
+  {
+    name: 'Venonat',
+    height: 10,
+    types: ['Bug', 'Poison'],
+  },
+  {
+    name: 'Primeape',
+    height: 10,
+    types: 'Fighting'
   }
 ];
+
+for (let i=0; i < pokemonlist.length; i++) {
+  if (pokemonlist[i].height > 13) {
+      document.write(`<p><b> ${pokemonlist[i].name} (height: ${pokemonlist[i].height}") - Wow, that’s big! </b></p>`);
+  } else {
+      document.write(`<p> ${pokemonlist[i].name} (height: ${pokemonlist[i].height}")</p>`);
+  }
+}
