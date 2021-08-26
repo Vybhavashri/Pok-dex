c36c966f15a80bb45d4d919b0feb40ac2347dea0
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2021-08-26 03:47:36 +0200

    Update script.js
    If applied, this commit will have pokemonList array wrapped in an IIFE variable with add and display functions. Also used foreachloop  instead of for loop.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,61 @@
 let pokemonlist = [];
-pokemonlist = [
-  {
-    name: 'Bulbasaur',
-    height: 7,
-    types: ['Grass', 'Poison']
-  },
-  {
-    name: 'Charizard',
-    height: 17,
-    types: ['Fire', 'Flying'],
-  },
-  {
-    name: 'Clefairy',
-    height: 6,
-    types: 'Fairy'
-  },
-  {
-    name: 'Spearow',
-    height: 3,
-    types: ['Flying', 'Normal']
-  },
-  {
-    name: 'Venonat',
-    height: 10,
-    types: ['Bug', 'Poison'],
-  },
-  {
-    name: 'Primeape',
-    height: 10,
-    types: 'Fighting'
-  }
-];
+let pokemonRepository = (function () {
+  pokemonlist = [
+    {
+      name: 'Bulbasaur',
+      height: 7,
+      types: ['Grass', 'Poison']
+    },
+    {
+      name: 'Charizard',
+      height: 17,
+      types: ['Fire', 'Flying'],
+    },
+    {
+      name: 'Clefairy',
+      height: 6,
+      types: 'Fairy'
+    },
+    {
+      name: 'Spearow',
+      height: 3,
+      types: ['Flying', 'Normal']
+    },
+    {
+      name: 'Venonat',
+      height: 10,
+      types: ['Bug', 'Poison'],
+    },
+    {
+      name: 'Primeape',
+      height: 10,
+      types: 'Fighting'
+    }
+  ];
 
-for (let i=0; i < pokemonlist.length; i++) {
-  if (pokemonlist[i].height > 13) {
-      document.write(`<p><b> ${pokemonlist[i].name} (height: ${pokemonlist[i].height}") - Wow, that’s big! </b></p>`);
-  } else {
-      document.write(`<p> ${pokemonlist[i].name} (height: ${pokemonlist[i].height}")</p>`);
+  function add(pokemon) {
+    if (typeof pokemon === "object") {
+      pokemonlist.push(pokemon);
+    }
   }
-}
+
+  function getAll() {
+    return pokemonlist;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.add({ name: 'Pikachu', height: 4, types: 'Electric' });
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(list) {
+  if (list.height > 13) {
+    document.write(`<p><b> ${list.name} (height: ${list.height}") - Wow, that’s big! </b></p>`);
+  } else {
+    document.write(`<p> ${list.name} (height: ${list.height}")</p>`);
+  }
+});
